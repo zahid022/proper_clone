4bf55bf84fb818ddff137e2ce7b4a8f1b4cfb37a
zahid022 <zahid.hzade001@gmail.com>
2025-01-03 14:44:54 +0400

    her section img

--- src/components/public/home/HeroSection.vue
+++ src/components/public/home/HeroSection.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-import imgMobile from '@/assets/img/hero_mb.jpg'
-import imgDesktop from '@/assets/img/hero_dk.jpg'
+
 </script>
 
 <template>
     <section class="min-h-[70vh] relative max-h-[87.5vh] overflow-hidden">
         <picture>
-            <source media="(min-width: 850px)" :srcset="imgDesktop" >
-            <img  class="object-cover" :src="imgMobile" alt="image" width="2000" height="1300">
+            <source media="(min-width: 850px)" srcset="https://res.cloudinary.com/djlix30nq/image/upload/v1735901038/uploads/1735901037794-hero_dk.jpg.jpg" >
+            <img  class="object-cover" src="https://res.cloudinary.com/djlix30nq/image/upload/v1735900997/uploads/1735900996350-hero_mb.jpg.jpg" alt="image" width="2000" height="1300">
         </picture>
         <div class="absolute left-3 bottom-6 md:bottom-10 lg:bottom-20 md:left-8 lg:left-12 z-[900]">
             <h2 class="text-[30px] md:text-[36px] font-serif text-white mb-4 tracking-wider font-normal">Fresh Fits</h2>
